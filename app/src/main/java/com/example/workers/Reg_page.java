0e2a08e4d8a1ea8bf2585578a6658d11734bfb61
Mayur Wayzade <mayurwayzade26@gmail.com>
2022-11-20 13:47:29 +0530

    Mayur

--- app/src/main/java/com/example/workers/Reg_page.java
+++ app/src/main/java/com/example/workers/Reg_page.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,11 +22,18 @@ public class Reg_page extends AppCompatActivity {
         setContentView(R.layout.activity_reg_page);
         getSupportActionBar().hide();
 
+
+        startActivity(new Intent(getApplicationContext(),HomeNavigation.class));
+
+        // Button
         Tv_Reg_page_Card1_next_btn=findViewById(R.id.tv_reg_page_cd1_nextbtn);
         Tv_Reg_page_Card2_next_btn=findViewById(R.id.tv_reg_page_cd2_nextbtn);
+
+        // CardView
         reg_page_cd1=findViewById(R.id.reg_page_cd_1);
         reg_page_cd2=findViewById(R.id.reg_page_cd_2);
         reg_page_cd3=findViewById(R.id.reg_page_cd_3);
+
 
         Tv_Reg_page_Card1_next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +46,7 @@ public class Reg_page extends AppCompatActivity {
         Tv_Reg_page_Card2_next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 reg_page_cd2.setVisibility(View.GONE);
                 reg_page_cd3.setVisibility(View.VISIBLE);
             }
